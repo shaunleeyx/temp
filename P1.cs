1899ccff2d6c951cfae231ffa0024d05339d4c0d
Shaun Lee <shaunleeyx@gmail.com>
2020-04-15 22:47:31 -0700

    P1 and ps1

--- P1.cs
+++ P1.cs
@@ -1,26 +1,62 @@
 /* 
  * Author:Shaun Lee
- * Driver Overview:
+ * Driver Overview: Tests out the functionality of Sensor.cs
+ * Additional Note: In the output the number on the right side of isValid is the battery after isValid
  */
 using System;
 class MainClass 
 {
+        static int SIZE = 10;
         public static void Main(string[] args)
         {
-                Sensor myObj = new Sensor();
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                Console.WriteLine(myObj.isValid());
-                myObj.Recharge();
-                Console.WriteLine(myObj.isValid());
-
+                Sensor[] arr = getSensorArr();
+                for(int i = 0; i < SIZE; i++)
+                {
+                        Console.WriteLine("Battery " + i + " initial battery " + arr[i].getBattery());
+                        testisValid(arr[i]);
+                        testRecharge(arr[i]);
+                } 
+                
         }
+        /*
+         *Precondition:none 
+         *Postcondition: returns an array of sensor objects
+         */
+        
+        static Sensor[] getSensorArr()
+        { 
+                Sensor[] arr = new Sensor[SIZE];
+                for(int i = 0; i < SIZE; i++)
+                {
+                        arr[i] = new Sensor();
+                }
+                return arr;
+        }
+         /*
+         *Precondition:sensor obj
+         *Postcondition: tests out isValid() function
+         */
+
+        static void testisValid(Sensor obj)
+        {
+                int count = 0;
+                while(obj.getState())
+                {
+                        Console.WriteLine("isValid: " + obj.isValid() + " Battery:" + obj.getBattery());
+                        count++;
+                }
+                Console.WriteLine("usage: " + count + "\n");
+        }
+         /*
+         *Precondition:sensor obj
+         *Postcondition: tests out recharge function
+         */
+
+        static void testRecharge(Sensor obj)
+        {
+                Console.WriteLine("recharging Sensor takes 2 seconds...");
+                obj.Recharge();
+                Console.WriteLine("Is sensor on? " + obj.getState() + ". testing isValid: " + obj.isValid() + "\n");
+        }
+
 }
---
+++ SensorTest/SensorTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+[TestClass]
+public class SensorTest
+{
+    [TestMethod]
+    public void CheckifBatteryIsOnInitially()
+    {
+        Sensor obj = new Sensor();
+        Assert.IsTrue(obj.getState());
+    }
+
+    [TestMethod]
+    public void CheckIfFirstisValidCallIsTrue()
+    {
+        Sensor obj = new Sensor();
+        Assert.IsTrue(obj.isValid());
+    }
+
+    [TestMethod]
+    public void CheckIfBatterycanBeDischarged()
+    {
+        Sensor obj = new Sensor();
+        while(obj.getState())
+        {
+            obj.isValid();
+        }
+        Assert.IsFalse(obj.isValid());
+    }
+
+    [TestMethod]
+    public void TestRecharge()
+    {
+        Sensor obj = new Sensor();
+        while (obj.getState())
+        {
+            obj.isValid();
+        }
+        obj.Recharge();
+        Assert.IsTrue(obj.isValid());
+
+    }
+}
