fa11434b4c36052e399075b1d532bec9279c2186
Shaun Lee <shaunleeyx@gmail.com>
2020-05-06 19:23:41 -0700

    saas

---
+++ target folder/robot.cs
@@ -0,0 +1,234 @@
+﻿//====================================================================================================
+//The Free Edition of C++ to C# Converter limits conversion output to 100 lines per file.
+
+//To purchase the Premium Edition, visit our website:
+//https://www.tangiblesoftwaresolutions.com/order/order-cplus-to-csharp.html
+//====================================================================================================
+
+//
+// Created by Shaun Lee on 2020-04-22.
+//
+
+/**
+ * Author:Shaun Lee
+ * Platform:CLion
+ * Class Overview:This class is the robot that can move around the maze
+ *
+ * Class Invariant:
+ *                     for first arg in move() inputting a 0 = north, 1 = south, 2 = right, 3 = left
+ *                     Robot is always turned on
+ *                     rCoord and cCoord is the row,column coordinates for the robot's position
+ *                     able to be copied
+ *                     isPowered returns the state of object (on/off)
+ *                     getRow and getColumn returns the coordinates for rCoord and cCoord
+ *                     size of the maze has to be always 11x11
+ *                     robot cannot get outside the maze
+ *
+ * Interface Invariant:
+ *                     robot has 4 actuator for each direction
+ *                     takes in a file through 1st argument in param
+ *                     2nd argument is drainRate for the battery for the Sensor
+ *                     if drainRate < 0 it will default to 1 if higher than 100 it will default to 1
+ *                     Recharge recharges the battery for the sensor
+ *                     inputting more than 4 will make hte function do nothing
+ **/
+
+public class robot : System.IDisposable
+{
+	/*
+	 *Precondition:string and double
+	 *Postcondition:filename is str and 2d array copies the file,double sets the drainRate for the battery for the sensor
+	 */
+	public robot(string str, double drainRate)
+	{
+		sObj = new sensor(drainRate);
+		nAct = new actuator(0);
+		sAct = new actuator(1);
+		eAct = new actuator(2);
+		wAct = new actuator(3);
+		rCoord = 5;
+		cCoord = 5;
+		state = true;
+		string text;
+		std::ifstream file = new std::ifstream(str);
+		if (file.is_open())
+		{
+		  for (int r = 0; r <= 10; r++)
+		  {
+			  for (int c = 0; c <= 10; c++)
+			  {
+				  string temp;
+				  file >> temp;
+				  grid[r][c] = stoi(temp);
+			  }
+		  }
+		file.close();
+		}
+	}
+
+	/*
+	 *Precondition: int
+	 *Postcondition:returns true or false
+	 */
+	public bool isValid(int num)
+	{
+		return sObj.isValid(num);
+	}
+
+	/*
+	 *Precondition:int from 0 to 4
+	 *Postcondition: moves the robot in direction depending on dir until a wall is in front
+	 */
+	public void Move(int Dir)
+	{
+		int sensed;
+		switch (Dir)
+		{
+			case 0:
+				sensed = grid[rCoord - 1][cCoord];
+				while (sObj.isValid(sensed))
+				{
+					nAct.MoveForward(ref rCoord, ref cCoord);
+					sensed = grid[rCoord - 1][cCoord];
+				}
+				break;
+			case 1:
+				sensed = grid[rCoord + 1][cCoord];
+				while (sObj.isValid(sensed))
+				{
+					sAct.MoveForward(ref rCoord, ref cCoord);
+					sensed = grid[rCoord + 1][cCoord];
+				}
+				break;
+			case 2:
+				sensed = grid[rCoord][cCoord + 1];
+				while (sObj.isValid(sensed))
+				{
+					eAct.MoveForward(ref rCoord, ref cCoord);
+					sensed = grid[rCoord][cCoord + 1];
+				}
+				break;
+			case 3:
+				sensed = grid[rCoord][cCoord - 1];
+				while (sObj.isValid(sensed))
+				{
+					wAct.MoveForward(ref rCoord, ref cCoord);
+					sensed = grid[rCoord][cCoord - 1];
+				}
+				break;
+			default:
+				return;
+		}
+	}
+
+	/*
+	 *Precondition: int from 0 to 4
+	 *Postcondition:moves the robot once
+	 */
+	public void MoveOne(int Dir)
+	{
+		int sensed;
+		switch (Dir)
+		{
+			case 0:
+				sensed = grid[rCoord - 1][cCoord];
+				if (sObj.isValid(sensed))
+				{
+					nAct.MoveForward(ref rCoord, ref cCoord);
+				}
+				break;
+			case 1:
+				sensed = grid[rCoord + 1][cCoord];
+				if (sObj.isValid(sensed))
+				{
+					sAct.MoveForward(ref rCoord, ref cCoord);
+				}
+				break;
+			case 2:
+				sensed = grid[rCoord][cCoord + 1];
+				if (sObj.isValid(sensed))
+				{
+					eAct.MoveForward(ref rCoord, ref cCoord);
+				}
+				break;
+			case 3:
+				sensed = grid[rCoord][cCoord - 1];
+				if (sObj.isValid(sensed))
+				{
+					wAct.MoveForward(ref rCoord, ref cCoord);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	/*
+	 *Precondition: state is set to something
+	 *Postcondition: returns the state of object
+	 */
+	public bool isPowered()
+	{
+		return state;
+	}
+
+	/*
+	 *Precondition: none
+	 *Postcondition:recharges the battery of the sensor to 100
+	 */
+	//only moves if it has 1
+
+	public void Recharge()
+	{
+		sObj.Recharge();
+	}
+
+	public void Dispose()
+	{
+	}
+
+	/*
+	 *Precondition: existing robot
+	 *Postcondition: copies the object robots
+	 */
+	public robot(robot obj)
+	{
+		rCoord = obj.rCoord;
+		cCoord = obj.cCoord;
+		state = obj.state;
+		sObj = obj.sObj;
+		nAct = obj.nAct;
+		sAct = obj.sAct;
+		wAct = obj.wAct;
+		eAct = obj.eAct;
+		for (int i = 0; i <= 10; i++)
+		{
+			for (int j = 0; j <= 10; j++)
+			{
+				grid[i][j] = obj.grid[i][j];
+			}
+		}
+	}
+
+	/*
+	 *Precondition: existing robot
+	 *Postcondition:transfer ownership of the robot
+	 */
+//C++ TO C# CONVERTER TODO TASK: 'rvalue references' have no equivalent in C#:
+	public robot(robot && obj)
+	{
+		swap(ref sObj, ref obj.sObj);
+		swap(ref rCoord, ref obj.rCoord);
+		swap(ref cCoord, ref obj.cCoord);
+		swap(ref state, ref obj.state);
+		swap(ref nAct, ref obj.nAct);
+		swap(ref sAct, ref obj.sAct);
+		swap(ref wAct, ref obj.wAct);
+/*Implementation invariant:
+ * echos sensor's isvalid
+ * echos actuator's moveforward() in move and moveone
+ * copies the file to a 2d array and row and column coordinates are the same
+ * move semantics uses swap for both = and the default move constructor
+ * MoveOne uses sensor and detects for walls make sure the robot doesn't overlap with the wall
+ * Move does the same thing but uses a while loop that invokes moveForward from the actuator until theres a wall in front
+ */
---
+++ target folder/sensor.cs
@@ -0,0 +1,116 @@
+﻿/**
+ * Author:Shaun Lee
+ * Platform:CLion
+ * Class Overview:This class is for a sensor that sense something that hasn't been implmented probably in p2. For now it
+ *                just works if the battery is above a certain threhold
+ *
+ * Class Invariant:  battery cannot be negative. If the battery is below 0 it will default to 0.
+ *                   the number variables are all doubles so don't overflow beyond what a double can hold
+ *                   isValid has nothing passed in param so its hard to mutate it
+ *                   min is the lowest drain rate it can be
+ *                   max is the highest number that the drain rate can be
+ *                   drain rate is randomly generated between min and max
+ *
+ * Interface Invariant:     implemented a get call for the client. Because I think it is necessary for the client to know what battery is the sensor at. just like this laptop I'm using
+ *                          State is essentially if the sensor is on or off. On is true and off is false.
+ *                          getState is a public functionality for the client to know whether the sensor is off or on
+ *                          battery starts off at 100% when the sensor is created
+ *                          drain() will not work if the sensor is turned off
+ **/
+
+
+
+
+public class sensor
+{
+
+	//TODO don't forget to remove this function
+	public double getdrainRate()
+	{
+		return drainRate;
+	}
+
+	public sensor(double input)
+	{
+		if (input > 100 || input < 0)
+		{
+			input = 1; //document this design decision
+		}
+		battery = 100;
+		drainRate = input;
+		state = true;
+	}
+
+	/*
+	*Precondition: initialized class
+	*Postcondition: decreases the battery based on the drainRate.
+	*/
+	public void drain()
+	{
+		if (battery == 0 || !state)
+		{
+			return;
+		}
+		battery -= drainRate;
+		if (battery < 0)
+		{
+			battery = 0;
+		}
+	}
+
+	/*
+	*Precondition:None
+	*Postcondition: returns true or false based on the battery
+	 */
+	public bool isValid(int digit)
+	{
+		checkBattery();
+		if (state)
+		{
+			drain();
+			return (digit == 1);
+		}
+		return false;
+	}
+
+	/*
+	   *Precondition:none
+	   *Postcondition: changes the state to true and battery to 100
+	   */
+	public void Recharge()
+	{
+		std::this_thread.sleep_for(std::chrono.milliseconds(200));
+		battery = 100;
+		state = true;
+	}
+
+	public double getBattery()
+	{
+		return battery;
+	}
+
+	private bool state;
+	private double battery;
+	private double drainRate;
+	/*
+	 *Precondition:none
+	 *Postcondition:changes the state of state if battery is at 10% or below
+	 */
+	private void checkBattery()
+	{
+		if (battery <= 10)
+		{
+			state = false;
+		}
+	}
+}
+/*Implementation invariant:
+ * Recharge has a 2 second delay using thread.sleep(2000)
+ * getBattery and getState doesn't mutate the data. So its okay functionality for client to have
+ * isValid checks the battery every single time its called
+ * isValid returns a bool for this version. Will change in future projects.
+ * State is off/on of the sensor
+ * Sensor is turned off when battery is 10%
+ * Battery starts off at 100
+ * changed the drainRate of the battery for the sensor instead of internally generated. We pass in the drainRate through the sensor's constructor
+ */
